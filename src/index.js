ced10d73ede6922285f52dddef26592232d612ba
Sagar Patke <sagarpatke@gmail.com>
2016-04-28 17:47:14 +0530

    Implemented all positive scenarios.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,64 @@
+var q = require('q');
+
 var Injector = function() {
-  this.dependencyCache = {};
+  this.dependencies = {};
+  this.dependencyPromises = {};
 };
 
 Injector.prototype.add = function(name, dependency) {
-  var func = dependency.pop();
-  var done = (function(err, resolve) {
-    this.dependencyCache[name] = resolve;
-  }).bind(this);
-  return func(done);
+  return this.dependencies[name] = dependency;
 };
 
 Injector.prototype.invoke = function(dependencies) {
   var func = dependencies.pop();
+  var promises = dependencies.map((function(name) {
+    return this.getValueOfDependency(name);
+  }).bind(this));
 
-  func(this.dependencyCache[dependencies[0]]);
+  return q.spread(promises, func);
+};
+
+Injector.prototype.getValueOfDependency = function(name) {
+  // If promise is not created, create promise.
+  if(!this.dependencyPromises.hasOwnProperty(name)) {
+    this.dependencyPromises[name] = this.createPromiseForDependency(name);
+  }
+
+  // Grab value from promise.
+  var deferred = q.defer();
+  this.dependencyPromises[name].then(deferred.resolve);
+
+  return deferred.promise;
+};
+
+Injector.prototype.createPromiseForDependency = function(name) {
+
+  var dependencies = this.dependencies[name];
+  var func = dependencies.pop();
+
+  var deferred = q.defer();
+  var done = (function(err, resolve) {
+    if(err) { /* TODO: Handle Error */ return;}
+    return deferred.resolve(resolve);
+  }).bind(this);
+
+  if(dependencies.length === 0) {
+    func(done);
+
+    return deferred.promise;
+  }
+
+  var promises = dependencies.map((function(name) {
+    return this.getValueOfDependency(name);
+  }).bind(this));
+
+  q.spread(promises, function() {
+    var args = Array.from(arguments);
+    args.push(done);
+    func.apply(null, args);
+  });
+
+  return deferred.promise;
 };
 
 module.exports = Injector;
--- test/basic-di.spec.js
+++ test/basic-di.spec.js
@@ -2,21 +2,175 @@ var Injector = require('../src');
 var should = require('should');
 
 describe('Async Dependency Injection', function() {
-
-  // FIXME: Implementation can assume that there is only one dependency.
-  // FIXME: Implementation can assume that add is receiving a synchronous function.
-  // FIXME: Implementation can assume that dependencies can be injected as soon as they have been added.
   it('Basic Scenario', function(done) {
     var injector = new Injector();
 
     var rand = (Math.random()*99387593793);
+    var count = 0;
 
     injector.add('thing1', [function(done) {
-      return done(null, rand);
+      setTimeout(function() {
+        count++;
+        return done(null, rand);
+      },1000);
     }]);
 
+    count.should.be.exactly(0);
+
     injector.invoke(['thing1', function(thing1) {
+      count.should.be.exactly(1);
       thing1.should.be.exactly(rand);
+      done();
+    }]);
+  });
+
+  // TODO: Implementation can assume that there is only one dependency.
+  it('Multiple Dependencies Invocation', function(done) {
+    var injector = new Injector();
+
+    var rand1 = (Math.random()*958728493827);
+    var rand2 = (Math.random()*958728493827);
+    var count1 = 0;
+    var count2 = 0;
+
+    injector.add('thing1', [function(done) {
+      setTimeout(function() {
+        count1++;
+        return done(null, rand1);
+      }, 1000);
+    }]);
+
+    injector.add('thing2', [function(done) {
+      setTimeout(function() {
+        count2++;
+        return done(null, rand2);
+      });
+    }]);
+
+    count1.should.be.exactly(0);
+    count2.should.be.exactly(0);
+
+    injector.invoke(['thing1', 'thing2', function(t1, t2) {
+      t1.should.be.exactly(rand1);
+      t2.should.be.exactly(rand2);
+      count1.should.be.exactly(1);
+      count2.should.be.exactly(1);
+      done();
+    }]);
+  });
+
+  it('Nested Dependency Invocation', function(done) {
+    var injector = new Injector();
+
+    var rand1 = Math.random() * 1837953092;
+    var rand2 = Math.random() * 5849382729;
+
+    var count1 = 0;
+    var count2 = 0;
+
+    injector.add('thing1', [function(done) {
+      setTimeout(function() {
+        count1++;
+        return done(null, rand1);
+      }, 300);
+    }]);
+
+    injector.add('thing2', ['thing1', function(t1, done) {
+      setTimeout(function() {
+        count2++;
+        return done(null, t1+rand2);
+      },1000);
+    }]);
+
+    count1.should.be.exactly(0);
+    count2.should.be.exactly(0);
+
+    injector.invoke(['thing2', function(t1) {
+      t1.should.be.exactly(rand1 + rand2);
+      count1.should.be.exactly(1);
+      count2.should.be.exactly(1);
+      done();
+    }]);
+  });
+
+  it('Nested Multiple Dependencies Invocation', function(done) {
+    var injector = new Injector();
+
+    var rand1 = Math.random() * 1837953092;
+    var rand2 = Math.random() * 5849382729;
+
+    var count1 = 0;
+    var count2 = 0;
+
+    injector.add('thing1', [function(done) {
+      setTimeout(function() {
+        count1++;
+        return done(null, rand1);
+      }, 300);
+    }]);
+
+    injector.add('thing2', ['thing1', function(t1, done) {
+      setTimeout(function() {
+        count2++;
+        return done(null, t1+rand2);
+      },1000);
+    }]);
+
+    count1.should.be.exactly(0);
+    count2.should.be.exactly(0);
+
+    injector.invoke(['thing2', 'thing1', function(t1,t2) {
+      t1.should.be.exactly(rand1 + rand2);
+      t2.should.be.exactly(rand1);
+      count1.should.be.exactly(1);
+      count2.should.be.exactly(1);
+      done();
+    }]);
+  });
+
+  it('Extra Nested Multiple Dependencies', function(done) {
+    var injector = new Injector();
+
+    var rand1 = Math.random() * 1837953092;
+    var rand2 = Math.random() * 5849382729;
+    var rand3 = Math.random() * 5838308403;
+
+    var count1 = 0;
+    var count2 = 0;
+    var count3 = 0;
+
+    injector.add('thing1', [function(done) {
+      setTimeout(function() {
+        count1++;
+        return done(null, rand1);
+      }, 300);
+    }]);
+
+    injector.add('thing2', ['thing1', function(t1, done) {
+      setTimeout(function() {
+        count2++;
+        return done(null, t1+rand2);
+      },400);
+    }]);
+
+    injector.add('thing3', ['thing1', 'thing2', function(t1,t2, done) {
+      setTimeout(function() {
+        count3++;
+        return done(null, t1+t2+rand3);
+      }, 100);
+    }]);
+
+    count1.should.be.exactly(0);
+    count2.should.be.exactly(0);
+    count3.should.be.exactly(0);
+
+    injector.invoke(['thing3', 'thing2', 'thing1', function(t3,t2,t1) {
+      t1.should.be.exactly(rand1);
+      t2.should.be.exactly(rand1+rand2);
+      t3.should.be.exactly(2*rand1+rand2+rand3);
+      count1.should.be.exactly(1);
+      count2.should.be.exactly(1);
+      count3.should.be.exactly(1);
       done();
     }]);
   });
